20eb2aef788f13feb7e3fa0f631bfafae0c1d11a
Varun Singh <varunsingh7004@gmail.com>
2025-08-13 05:42:54 +0530

    Pre cleanup

--- Backend/models/captain.model.js
+++ Backend/models/captain.model.js
@@ -87,15 +87,6 @@ captainSchema.methods.comparePassword = async function (password) {
 }
 
 
-captainSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-
-
 const captainModel = mongoose.model('captain', captainSchema)
 
 
